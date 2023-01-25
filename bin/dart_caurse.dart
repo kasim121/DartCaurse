536c462068aecd3366f92d401a29d4ce37d5aaae
Kasim <kasimplecoder@gmai.com>
2023-01-25 18:04:01 +0530

    first commit

---
+++ bin/dart_caurse.dart
@@ -0,0 +1,5 @@
+import 'package:dart_caurse/dart_caurse.dart' as dart_caurse;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_caurse.calculate()}!');
+}
---
+++ test/dart_caurse_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_caurse/dart_caurse.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
